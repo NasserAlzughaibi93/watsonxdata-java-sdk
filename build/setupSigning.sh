410fdb81bc7b2ce66624d377cbeb7f68ea8923ac
fanfei <fanfei@cn.ibm.com>
2023-07-12 20:10:37 -0700

    fix: update sign

--- build/setupSigning.sh
+++ build/setupSigning.sh
@@ -10,7 +10,8 @@ echo "Importing signing key..."
 
 # Modify the command below to use the correct environment variables
 # that were added to your Travis build settings when you encrypted your signing.key file.
-openssl aes-256-cbc -K $encrypted_4c2ca4326cdc_key -iv $encrypted_4c2ca4326cdc_iv -in build/signing.key.enc -out build/signing.key -d
+#openssl aes-256-cbc -K $encrypted_4c2ca4326cdc_key -iv $encrypted_4c2ca4326cdc_iv -in build/signing.key.enc -out build/signing.key -d
+openssl aes-256-cbc -K $encrypted_27bbc6d48ea2_key -iv $encrypted_27bbc6d48ea2_iv -in build/signing.key.enc -out build/signing.key -d
 
 gpg --version
 gpg --batch --import build/signing.key
